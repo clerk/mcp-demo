b75b88f1c6fbb7c0694b98d3c863e5c48636cbdb
Andrew Qu <qual1337@gmail.com>
2025-05-15 16:50:42 -0700

    f

---
+++ resource-server/app/[transport]/route.ts
@@ -0,0 +1,76 @@
+import { z } from "zod";
+import { default as _debug } from "debug";
+import { auth } from "@clerk/nextjs/server";
+import { createMcpHandler } from "@vercel/mcp-adapter";
+
+const debug = _debug("mcp-demo-resource-server");
+
+const handler = async (req: Request) => {
+  debug("Request hit MCP endpoint route");
+
+  // Check for an Authorization header
+  if (!req.headers.get("Authorization")) {
+    debug("No authorization header, sending back an error");
+
+    // This is hard coded here, but we plan to make it such that if this
+    // request is forwarded to the actual resource endpoint without auth, we
+    // will return the value below automatically via Clerk's SDKs.
+    const wwwAuthenticateValue =
+      "Bearer resource_metadata=http://localhost:3001/.well-known/oauth-authorization-server";
+
+    return Response.json(
+      {
+        jsonrpc: "2.0",
+        error: {
+          code: -32000,
+          message: "Unauthorized: Authentication required",
+          // I'm taking this liberty since the official mcp client doesn't
+          // return headers when it gets an error response at the moment.
+          // https://github.com/modelcontextprotocol/typescript-sdk/blob/bced33d8bc57419c6d498ca9a26a284f3ccf6016/src/client/streamableHttp.ts#L414
+          "www-authenticate": wwwAuthenticateValue,
+        },
+        id: null,
+      },
+      {
+        status: 401,
+        headers: {
+          "WWW-Authenticate": wwwAuthenticateValue,
+        },
+      }
+    );
+    // biome-ignore lint/style/noUselessElse: <explanation>
+  } else {
+    debug("Verifying OAuth access token");
+
+    const { subject } = await auth({ acceptsToken: "oauth_token" });
+
+    if (!subject) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  }
+
+  return createMcpHandler(
+    (server) => {
+      server.tool(
+        "roll_dice",
+        "Rolls an N-sided die",
+        { sides: z.number().int().min(2) },
+        async ({ sides }) => {
+          debug("tool call executed");
+          const value = 1 + Math.floor(Math.random() * sides);
+          return {
+            content: [{ type: "text", text: `🎲 You rolled a ${value}!` }],
+          };
+        }
+      );
+    },
+    {},
+    {
+      basePath: "",
+    }
+  );
+};
+
+export { handler as GET };
+export { handler as POST };
+export { handler as DELETE };
