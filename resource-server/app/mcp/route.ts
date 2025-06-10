aff19073018527ca1ab13161e9d4a8211186f9a6
omerfe <omerfe.eker@gmail.com>
2025-06-10 03:31:06 +0300

    fix: replace nonexisted MachineAuthObject type with the actual return type from mcp-adapter, add missing auth server discovery endpoint, bump mcp-adapter

--- resource-server/app/mcp/route.ts
+++ resource-server/app/mcp/route.ts
@@ -3,7 +3,24 @@ import {
   experimental_withMcpAuth as withMcpAuth,
 } from "@vercel/mcp-adapter";
 import { auth, clerkClient } from "@clerk/nextjs/server";
-import { type MachineAuthObject } from "@clerk/backend";
+
+type AuthInfoFunc = Parameters<typeof withMcpAuth>[1];
+
+// this AuthInfo type should be exported from the @vercel/mcp-adapter package, juggling for now
+type AuthInfo = AuthInfoFunc extends (...args: any[]) => infer R
+  ? R extends Promise<infer P>
+    ? P
+    : R
+  : never;
+
+function checkAuth(authInfo: AuthInfo) {
+  const subject = authInfo?.extra?.subject as string | undefined;
+  if (!subject) {
+    console.error(authInfo);
+    throw new Error("Unauthenticated");
+  }
+  return subject;
+}
 
 const clerk = await clerkClient();
 
@@ -13,15 +30,8 @@ const handler = createMcpHandler((server) => {
     "Gets data about the Clerk user that authorized this request",
     {},
     async (_, { authInfo }) => {
-      const clerkAuthInfo =
-        authInfo as unknown as MachineAuthObject<"oauth_token">;
-      if (!clerkAuthInfo?.subject) {
-        console.error(authInfo);
-        return {
-          content: [{ type: "text", text: "Error: user not authenticated" }],
-        };
-      }
-      const user = await clerk.users.getUser(clerkAuthInfo.subject);
+      const subject = checkAuth(authInfo);
+      const user = await clerk.users.getUser(subject);
       return {
         content: [{ type: "text", text: JSON.stringify(user) }],
       };
@@ -32,8 +42,17 @@ const handler = createMcpHandler((server) => {
 const authHandler = withMcpAuth(
   handler,
   async (_, token) => {
-    if (!token) return null;
-    return await auth({ acceptsToken: "oauth_token" });
+    if (!token) return undefined;
+    const machineAuth = await auth({ acceptsToken: "oauth_token" });
+    if (!machineAuth.id) return undefined;
+    return {
+      token,
+      clientId: machineAuth.id,
+      scopes: machineAuth.scopes,
+      extra: {
+        subject: machineAuth.subject,
+      },
+    };
   },
   { required: true }
 );
